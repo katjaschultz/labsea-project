af9d2aa962e35e62c9add1060921b2a882e7a792
katjaschultz <126666528+katjaschultz@users.noreply.github.com>
2025-05-20 17:19:43 +0200

    small changes

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -11,6 +11,7 @@ import numpy as np
 matplotlib.use('agg')  # use agg backend to prevent creating plot windows during tests
 
 def test_calc_dyn_h():
+    # Test with a simple case if specvol_anom has only one vertical layer output should be zero
     specvol = np.ones((1, 3))
     p = np.ones((1, 3)) * 10
     dyn_h = tools.calc_dyn_h(specvol, p)
